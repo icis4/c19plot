8718c97b535084b606161c4ab363b7a9bf95ad7e
Ivaylo Iltchev <github@iltchev.com>
2020-07-06 19:50:45 +0300

    update 2020-07-06

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # c19plot
-Python, Plot data from https://opendata.ecdc.europa.eu/covid19/casedistribution/json/ 
+Python, Plot data from https://opendata.ecdc.europa.eu/covid19/casedistribution/json/
+
+![Result 2020-07-06](img/plot-2020-07-06.png)
 
 ![Result 2020-04-08](img/s19.png)
--- src/c19plot.py
+++ src/c19plot.py
@@ -19,22 +19,32 @@ url = r"https://opendata.ecdc.europa.eu/covid19/casedistribution/json/"
 response = request.urlopen(url)
 data = json.loads(response.read())['records']
 
-plt.ylabel('cases/deaths')
+plt.ylabel('cases/deaths by pupulation 2019')
 plt.grid(True)
 
-for i, country in enumerate(['BG','IT', 'FR', 'US', 'CN']):
+for i, country in enumerate(['BG','RU', 'US']):#'IT', 'FR', 'US', 'CN']):
     country_data = list(filter(lambda x: x['geoId'] == country, data))
     result = list(map(lambda x: (datetime.date(*list(reversed(list( \
         map(int, x['dateRep'].split("/")))))), int(x['cases']), \
-        int(x['deaths'])), country_data))
+        int(x['deaths']), int(x['popData2019'])), country_data))
 
     dateRep = np.flip(np.array(list(map(lambda x: x[0].strftime("%Y-%m-%d"), \
         result)), dtype='datetime64[D]'))
     cases = np.flip(np.array(list(map(lambda x: x[1], result)), dtype='i'))
     deaths = np.flip(np.array(list(map(lambda x: x[2], result)), dtype='i'))
+    popd = np.flip(np.array(list(map(lambda x: x[3], result)), dtype='i'))
 
-    plt.plot(dateRep, np.cumsum(cases), dateRep, np.cumsum(deaths), \
-        label=country)
+    plt.subplot(2, 1, 1)
+    plt.plot(dateRep, cases/popd * 1e6, label = country + "-cases per milion")
+    plt.subplot(2, 1, 2)
+    plt.plot(dateRep, deaths/popd * 1e6, label = country + "-deaths per milion")
 
+plt.subplot(2, 1, 1)
+plt.title(r"https://opendata.ecdc.europa.eu/covid19/casedistribution/json/")
 plt.legend()
+plt.grid(True)
+plt.subplot(2, 1, 2)
+plt.legend()
+plt.grid(True)
+
 plt.show()
