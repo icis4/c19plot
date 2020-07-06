559493d5cd95cbee168edf5564d039e69f29b8a2
Ivaylo Iltchev <github@iltchev.com>
2020-07-06 20:43:39 +0300

    numbers representation more countries

--- src/c19plot.py
+++ src/c19plot.py
@@ -19,7 +19,7 @@ url = r"https://opendata.ecdc.europa.eu/covid19/casedistribution/json/"
 response = request.urlopen(url)
 data = json.loads(response.read())['records']
 
-for i, country in enumerate(['BG','RU', 'US']):#'IT', 'FR', 'US', 'CN']):
+for i, country in enumerate(['BG','RU', 'US', 'IT', 'CN']):
     country_data = list(filter(lambda x: x['geoId'] == country, data))
     result = list(map(lambda x: (datetime.date(*list(reversed(list( \
         map(int, x['dateRep'].split("/")))))), int(x['cases']), \
@@ -27,14 +27,14 @@ for i, country in enumerate(['BG','RU', 'US']):#'IT', 'FR', 'US', 'CN']):
 
     dateRep = np.flip(np.array(list(map(lambda x: x[0].strftime("%Y-%m-%d"), \
         result)), dtype='datetime64[D]'))
-    cases = np.flip(np.array(list(map(lambda x: x[1], result)), dtype='i'))
-    deaths = np.flip(np.array(list(map(lambda x: x[2], result)), dtype='i'))
-    popd = np.flip(np.array(list(map(lambda x: x[3], result)), dtype='i'))
+    cases = np.flip(np.array(list(map(lambda x: x[1], result)), dtype='d'))
+    deaths = np.flip(np.array(list(map(lambda x: x[2], result)), dtype='d'))
+    popd = np.flip(np.array(list(map(lambda x: x[3], result)), dtype='d'))
 
     plt.subplot(2, 1, 1)
-    plt.plot(dateRep, cases/popd * 1e6, label = country)
+    plt.plot(dateRep, cases/popd * 1.0e+6, label = country)
     plt.subplot(2, 1, 2)
-    plt.plot(dateRep, deaths/popd * 1e6, label = country)
+    plt.plot(dateRep, deaths/popd * 1.0e+6, label = country)
 
 plt.subplot(2, 1, 1)
 plt.title(r"https://opendata.ecdc.europa.eu/covid19/casedistribution/json/")
