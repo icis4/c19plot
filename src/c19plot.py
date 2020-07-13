f04b25318fcbbe493a79aaee40750d37e335dbd9
Ivaylo Iltchev <github@iltchev.com>
2020-07-13 13:33:24 +0300

    save to svg file

--- src/c19plot.py
+++ src/c19plot.py
@@ -14,11 +14,15 @@ import datetime
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import time
+import os
 
 plt.style.use('bmh')
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex = True)
-fig.subplots_adjust(left = 0.05, right = 0.95, top = 0.95, bottom = 0.05,\
+fig.subplots_adjust(left = 0.10, right = 0.90, top = 0.90, bottom = 0.10,\
     wspace = 0)
+fig.set_size_inches(10,8)
+
 ax1.set_title(\
     r"https://opendata.ecdc.europa.eu/covid19/casedistribution/json/")
 ax1.grid(color='#333333')
@@ -56,4 +60,15 @@ for i, country in enumerate(['BG', 'US', 'RU',]):
 
 ax1.legend()
 ax2.legend()
-plt.show()
+plt.savefig(
+    os.path.normpath(
+        os.path.join(
+            os.path.dirname(__file__),
+            "..",
+            "img",
+            time.strftime("plot-%Y-%m-%d.svg")
+        )
+    ),
+    format="svg"
+)
+#plt.show()
